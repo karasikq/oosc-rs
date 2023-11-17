ff6b56eeb873016826a5f5eb99d9352d3ff55496
karasikq <romastepanuik@gmail.com>
2023-11-17 23:07:55 +0300

    Generic evaluate, EvaluateMut trait

--- src/utils/evaluate.rs
+++ src/utils/evaluate.rs
@@ -1,5 +1,9 @@
 use crate::error::Error;
 
-pub trait Evaluate {
-    fn evaluate(&self, t: f32) -> Result<f32, Error>;
+pub trait Evaluate<T> {
+    fn evaluate(&self, t: f32) -> Result<T, Error>;
+}
+
+pub trait EvaluateMut<T> {
+    fn evaluate(&mut self, t: f32) -> Result<T, Error>;
 }
