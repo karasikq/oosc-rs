141d5e30f01a5678608af0f5dbbf62a95915558d
karasikq <romastepanuik@gmail.com>
2023-11-18 13:06:19 +0300

    Note_on feature

--- src/core/oscillator.rs
+++ src/core/oscillator.rs
@@ -16,20 +16,26 @@ pub struct Oscillator {
     wavetable: WaveTable,
 }
 
+impl Oscillator {
+    pub fn note_on(&mut self, note: Note) -> Result<(), Error> {
+        let mut notes = self.notes.lock().expect("Cannot lock notes");
+        notes.push(note);
+        Ok(())
+    }
+}
+
+/// TO-DO: Move hard-coded sample rate to settings
 impl EvaluateMut<Arc<Mutex<SampleBuffer>>> for Oscillator {
-    fn evaluate(&mut self, t: f32) -> Result<Arc<Mutex<SampleBuffer>>, Error> {
+    fn evaluate(&mut self, delta_time: f32) -> Result<Arc<Mutex<SampleBuffer>>, Error> {
         let mut buffer = self.buffer.lock().expect("Cannot lock buffer");
-        let delta_time = 1.0 / buffer.len() as f32;
-        let mut time = t;
         for i in 0..buffer.len() {
             let mut notes = self.notes.lock().expect("Cannot lock notes");
             for note in notes.iter_mut() {
-                let samples = EvaluateWithParam::evaluate(self, time, note)?;
+                let samples = EvaluateWithParam::evaluate(self, note.play_time, note)?;
                 buffer.set_at(0, i, samples.0)?;
                 buffer.set_at(1, i, samples.1)?;
                 note.play_time += delta_time;
             }
-            time += delta_time;
         }
         Ok(self.buffer.clone())
     }
@@ -38,23 +44,23 @@ impl EvaluateMut<Arc<Mutex<SampleBuffer>>> for Oscillator {
 impl EvaluateWithParam<&Note, (f32, f32)> for Oscillator {
     fn evaluate(&self, t: f32, note: &Note) -> Result<(f32, f32), Error> {
         let envelope = match note.hold_on {
-            State::None => self.envelope.evaluate(note.play_time),
+            State::None => self.envelope.evaluate(t),
             _ => {
                 if t > self.envelope.time_range_of(note.hold_on).1 {
                     self.envelope.peak_at(note.hold_on)
                 } else {
-                    self.envelope.evaluate(note.play_time)
+                    self.envelope.evaluate(t)
                 }
             }
         };
-        let freq = PI_2M * note.frequency * note.play_time;
+        let freq = PI_2M * note.frequency * t;
         let sample = self
             .wavetable
             /* .lock()
             .expect("Cannot lock wavetable") */
             .evaluate(freq)?;
 
-        Ok((sample * envelope * 0.2, sample * envelope * 0.2))
+        Ok((sample * envelope * 1., sample * envelope * 0.2))
     }
 }
 
