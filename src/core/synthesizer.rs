01d7eeadbaada780cb42c2574fb2a74eb6f1c567
karasikq <romastepanuik@gmail.com>
2023-11-19 12:25:23 +0300

    One-osc synthesizer

--- src/core/synthesizer.rs
+++ src/core/synthesizer.rs
@@ -3,10 +3,7 @@ use std::sync::{Arc, Mutex};
 use super::{note::Note, oscillator::Oscillator};
 use crate::{
     error::Error,
-    utils::{
-        evaluate::{Evaluate, EvaluateMut},
-        sample_buffer::SampleBuffer,
-    },
+    utils::{evaluate::EvaluateMut, sample_buffer::SampleBuffer},
 };
 
 pub struct Synthesizer {
@@ -15,6 +12,13 @@ pub struct Synthesizer {
 }
 
 impl Synthesizer {
+    pub fn new(osc: Oscillator) -> Self {
+        Self {
+            notes: Arc::new(Mutex::new(Vec::<Note>::new())),
+            osc,
+        }
+    }
+
     pub fn note_on(&mut self, note: Note) -> Result<(), Error> {
         let mut notes = self.notes.lock().expect("Cannot lock notes");
         notes.push(note);
@@ -23,10 +27,10 @@ impl Synthesizer {
 }
 
 impl EvaluateMut<Arc<Mutex<SampleBuffer>>> for Synthesizer {
-    fn evaluate(&mut self, _: f32) -> Result<Arc<Mutex<SampleBuffer>>, Error> {
+    fn evaluate(&mut self, delta: f32) -> Result<Arc<Mutex<SampleBuffer>>, Error> {
         let mut notes = self.notes.lock().expect("Cannot lock notes");
         for note in notes.iter_mut() {
-            let t = self.osc.evaluate_note(note, 1. / 44100.)?;
+            let t = self.osc.evaluate_note(note, delta)?;
             note.play_time = t;
         }
         Ok(self.osc.get_buffer())
