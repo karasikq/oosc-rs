36f5d3a6c0107b5cf2fe415469f62d87b0d0b7da
karasikq <romastepanuik@gmail.com>
2023-11-29 20:32:10 +0300

    Remove lifetimes in mediators

--- src/midi/mediator.rs
+++ src/midi/mediator.rs
@@ -1,37 +1,40 @@
 use midly::TrackEvent;
 
 use crate::{
-    core::{note::Note, synthesizer::Synthesizer},
+    core::{
+        note::Note,
+        synthesizer::{SyncSynthesizer, Synthesizer},
+    },
     error::Error,
 };
 
-pub trait MidiEventReceiver<'a> {
+pub trait MidiEventReceiver {
     fn receive_event(&mut self, event: &TrackEvent) -> Result<(), Error>;
 }
 
-pub struct MidiSynthesizerMediator<'a> {
-    synthesizer: &'a mut Synthesizer,
+pub struct MidiSynthesizerMediator {
+    synthesizer: SyncSynthesizer,
 }
 
-impl<'a> MidiSynthesizerMediator<'a> {
-    pub fn new(synthesizer: &'a mut Synthesizer) -> Self {
+impl MidiSynthesizerMediator {
+    pub fn new(synthesizer: SyncSynthesizer) -> Self {
         Self { synthesizer }
     }
 }
 
-impl<'a> MidiEventReceiver<'a> for MidiSynthesizerMediator<'a> {
+impl MidiEventReceiver for MidiSynthesizerMediator {
     fn receive_event(&mut self, event: &TrackEvent) -> Result<(), Error> {
+        let mut syn = self.synthesizer.lock().unwrap();
         match event.kind {
             midly::TrackEventKind::Midi { message, .. } => match message {
                 midly::MidiMessage::NoteOn { key, vel } => {
                     let key = key.as_int();
                     let vel = vel.as_int();
-                    self.synthesizer
-                        .note_on(Note::new(key.into(), vel.into()))?;
+                    syn.note_on(Note::new(key.into(), vel.into()))?;
                 }
                 midly::MidiMessage::NoteOff { key, .. } => {
                     let key = key.as_int();
-                    self.synthesizer.note_off(key.into())?;
+                    syn.note_off(key.into())?;
                 }
                 _ => (),
             },
