ad12c8208b7c2da3cf8cdb7a7af6fc9b06e34a8b
karasikq <romastepanuik@gmail.com>
2023-11-30 22:33:12 +0300

    Test linear interpolation

--- src/utils/interpolation.rs
+++ src/utils/interpolation.rs
@@ -28,3 +28,18 @@ pub fn interpolate_lagrange(fx: Vec<InPoint>, xm: f32) -> f32 {
 pub fn interpolate_linear(y1: f32, y2: f32, fraction: f32) -> f32 {
     y1 + (y2 - y1) * fraction
 }
+
+
+#[cfg(test)]
+mod tests {
+    use assert_approx_eq::assert_approx_eq;
+
+    use crate::utils::interpolation::interpolate_linear;
+
+    #[test]
+    fn test_interpolation_linear() {
+        assert_approx_eq!(interpolate_linear(-0.25, 0.25, 0.5), 0.);
+        assert_approx_eq!(interpolate_linear(0., 1., 0.33), 0.33);
+        assert_approx_eq!(interpolate_linear(0., 0.5, 0.25), 0.125);
+    }
+}
