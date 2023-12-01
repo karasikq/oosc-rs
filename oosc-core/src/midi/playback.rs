3e32abbee1ea1340cff38e4003f7f97c69203f3c
karasikq <romastepanuik@gmail.com>
2023-12-01 11:11:16 +0300

    Playback state

--- oosc-core/src/midi/playback.rs
+++ oosc-core/src/midi/playback.rs
@@ -4,9 +4,19 @@ use crate::error::Error;
 
 use super::{mediator::MidiEventReceiver, smf_extensions::OwnedSmf};
 
+#[derive(Clone, Copy)]
+pub enum PlaybackState {
+    None,
+    Playing(f32),
+    Paused(f32),
+    Stoped,
+}
+
 pub trait PlaybackControl: Sync + Send {
     fn load(&mut self, data: Smf<'_>);
+    fn play(&mut self);
     fn set_bpm(&mut self, bpm: f32);
+    fn get_state(&self) -> PlaybackState;
     fn process_events(
         &mut self,
         delta_time: f32,
@@ -15,21 +25,30 @@ pub trait PlaybackControl: Sync + Send {
     fn reset(&mut self);
 }
 
-pub type BoxedMidiPlayback = Option<Box<dyn PlaybackControl>>;
-
 pub struct SmfPlayback {
     time: f32,
     midi_ticks: u32,
     tps: f32,
     bpm: f32,
     ppq: u32,
-    data: OwnedSmf,
+    data: Option<OwnedSmf>,
+    state: PlaybackState,
 }
 
-pub type OptionalPlayback = Option<SmfPlayback>;
-
 impl SmfPlayback {
-    pub fn new(data: Smf<'_>) -> Result<Self, Error> {
+    pub fn new() -> Self {
+        Self {
+            time: 0.0,
+            midi_ticks: 0,
+            tps: 0.0,
+            bpm: 0.0,
+            ppq: 0,
+            data: None,
+            state: PlaybackState::None,
+        }
+    }
+
+    pub fn from_smf(data: Smf<'_>) -> Result<Self, Error> {
         let data = OwnedSmf::try_from(&data)?;
         let ppq = match data.header.timing {
             Timing::Metrical(v) => v.into(),
@@ -43,12 +62,19 @@ impl SmfPlayback {
             tps,
             bpm,
             ppq: ppq.into(),
-            data,
+            data: Some(data),
+            state: PlaybackState::Stoped,
         })
     }
 
     fn calculate_tps(bpm: f32, ppq: u32) -> f32 {
         (bpm * ppq as f32) / 60.
+    }
+}
+
+impl Default for SmfPlayback {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -58,6 +84,7 @@ impl PlaybackControl for SmfPlayback {
         delta_time: f32,
         event_receiver: &mut Box<dyn MidiEventReceiver>,
     ) -> Result<(), Error> {
+        let data = self.data.as_ref().ok_or("Cannot get midi data for playback")?;
         let t = self.time + delta_time;
         let playback_time_ticks: u32 = (t * self.tps) as u32;
         let playback_midi_ticks: u32 = self.midi_ticks;
@@ -65,7 +92,7 @@ impl PlaybackControl for SmfPlayback {
         let mut last_event_ticks: u32 = playback_midi_ticks;
         let mut current_ticks: u32 = 0;
 
-        for track in self.data.tracks.iter().skip(1) {
+        for track in data.tracks.iter().skip(1) {
             for event in track.iter() {
                 current_ticks += event.delta;
                 if current_ticks > playback_midi_ticks + delta_ticks {
@@ -98,12 +125,14 @@ impl PlaybackControl for SmfPlayback {
     }
 
     fn reset(&mut self) {
+        self.data = None;
         self.time = 0.0;
         self.midi_ticks = 0;
+        self.state = PlaybackState::None;
     }
 
     fn load(&mut self, data: Smf<'_>) {
-        self.data = OwnedSmf::try_from(&data).unwrap();
+        self.data = Some(OwnedSmf::try_from(&data).unwrap());
         let ppq = match data.header.timing {
             Timing::Metrical(v) => v.into(),
             Timing::Timecode(_, _) => 192,
@@ -113,6 +142,14 @@ impl PlaybackControl for SmfPlayback {
         self.tps = Self::calculate_tps(self.bpm, self.ppq);
         self.midi_ticks = 0;
         self.time = 0.0;
+        self.state = PlaybackState::Stoped;
     }
 
+    fn play(&mut self) {
+        self.state = PlaybackState::Playing(self.time);
+    }
+
+    fn get_state(&self) -> PlaybackState {
+        self.state
+    }
 }
