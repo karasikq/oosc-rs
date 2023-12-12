7a778d518a570f13deb19efba4ae6898dfabd0ac
karasikq <romastepanuik@gmail.com>
2023-12-12 23:49:10 +0300

    Delay effect

--- oosc-core/src/effects/mod.rs
+++ oosc-core/src/effects/mod.rs
@@ -3,6 +3,7 @@ use crate::{error::Error, utils::sample_buffer::SampleBuffer};
 pub mod amplifier;
 pub mod compressor;
 pub mod sample_detector;
+pub mod delay;
 
 pub trait Effect {
     fn process(&mut self, buffer: &mut SampleBuffer) -> Result<(), Error>;
