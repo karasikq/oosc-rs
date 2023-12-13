b06c4090ed8282d28f7b9ad03e18f295de86e017
karasikq <romastepanuik@gmail.com>
2023-12-13 21:29:31 +0300

    Fix compessor & sample detector

--- oosc-core/src/effects/sample_detector.rs
+++ oosc-core/src/effects/sample_detector.rs
@@ -1,10 +1,10 @@
-use std::{rc::Rc, cell::RefCell};
+use std::sync::{Arc, RwLock};
 
-use crate::{core::parametrs::ExponentialTimeParametr, utils::convert::linear_to_voltage};
+use crate::{core::parametrs::ExponentialTimeParametr, utils::convert::{linear_to_voltage, linear_to_power}};
 
 use super::SampleProcessor;
 
-pub type TimeParametr = Rc<RefCell<ExponentialTimeParametr>>;
+pub type TimeParametr = Arc<RwLock<ExponentialTimeParametr>>;
 
 pub struct SampleDetector {
     attack: TimeParametr,
@@ -26,15 +26,15 @@ impl SampleProcessor for SampleDetector {
     fn process(&mut self, sample: f32) -> f32 {
         let sample = sample * sample;
         let time = if sample > self.last_output {
-            self.attack.borrow().exponential_time
+            self.attack.read().unwrap().exponential_time
         } else {
-            self.release.borrow().exponential_time
+            self.release.read().unwrap().exponential_time
         };
         self.last_output = time * (self.last_output - sample) + sample;
         if self.last_output <= 0.0 {
             -96.0
         } else {
-            linear_to_voltage(self.last_output)
+            linear_to_power(self.last_output)
         }
     }
 }
