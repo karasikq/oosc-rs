4a3fc455a4705aa955814d8d544b739717ceb904
karasikq <romastepanuik@gmail.com>
2023-12-24 20:10:53 +0300

    Move evaluate mut to Evaluate trait

--- oosc-core/src/utils/evaluate.rs
+++ oosc-core/src/utils/evaluate.rs
@@ -2,8 +2,7 @@ use crate::error::Error;
 
 pub trait Evaluate<T> {
     fn evaluate(&self, t: f32) -> Result<T, Error>;
-}
-
-pub trait EvaluateMut<T> {
-    fn evaluate(&mut self, t: f32) -> Result<T, Error>;
+    fn evaluate_mut(&mut self, t: f32) -> Result<T, Error> {
+        self.evaluate(t)
+    }
 }
