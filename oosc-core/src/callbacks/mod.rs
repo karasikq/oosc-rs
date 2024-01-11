401eb371b25fc7bc56d565deb7fe29eee3f98990
karasikq <romastepanuik@gmail.com>
2024-01-12 00:36:04 +0300

    Push sample_rate to process stream trait

--- oosc-core/src/callbacks/mod.rs
+++ oosc-core/src/callbacks/mod.rs
@@ -4,5 +4,5 @@ pub mod stream_callback;
 pub mod stream_renderer;
 
 pub trait StreamCallback: Send + Sync {
-    fn process_stream(&mut self, data: &mut [f32], time: f32) -> Result<(), Error>;
+    fn process_stream(&mut self, data: &mut [f32], time: f32, sample_rate: f32) -> Result<(), Error>;
 }
