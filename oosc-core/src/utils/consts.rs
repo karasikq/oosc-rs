bf768e339432c1bbf51e02a5addada995f33369a
karasikq <romastepanuik@gmail.com>
2024-01-23 20:01:49 +0300

    New consts

--- oosc-core/src/utils/consts.rs
+++ oosc-core/src/utils/consts.rs
@@ -4,3 +4,5 @@ pub const PI_2: f32 = std::f32::consts::PI * 0.5;
 pub const PI_4: f32 = std::f32::consts::PI * 0.25;
 pub const INVERSE_E: f32 = 1.0 / std::f32::consts::E;
 pub const ANALOG_TC: f32 = -0.999_672_35_f32; // ln(0.367)
+pub const SQRT_2: f32 = std::f32::consts::SQRT_2;
+pub const FRAC_1_SQRT_2: f32 = std::f32::consts::FRAC_1_SQRT_2;
