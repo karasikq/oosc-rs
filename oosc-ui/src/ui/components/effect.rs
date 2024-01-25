a9bb2060a556b3b3bcd91236d1db4ca5bd5dfcde
karasikq <romastepanuik@gmail.com>
2024-01-25 21:07:12 +0300

    Effect layout for components

--- oosc-ui/src/ui/components/effect.rs
+++ oosc-ui/src/ui/components/effect.rs
@@ -1,10 +1,12 @@
+use std::rc::Rc;
+
 use crossterm::event::KeyCode;
 use oosc_core::{
     effects::Effect,
     utils::{make_shared, Shared},
 };
 use ratatui::{
-    prelude::{Alignment, Direction, Margin, Rect},
+    prelude::{Alignment, Direction, Margin, Rect, Layout, Constraint},
     style::{Color, Style},
     text::Span,
     widgets::{Block, BorderType, Borders},
@@ -19,7 +21,7 @@ use super::{
 
 struct EffectLayout {
     pub rect: Rect,
-    pub inner: Rect,
+    pub inner: Rc<[Rect]>,
 }
 
 pub struct EffectComponent {
@@ -36,13 +38,15 @@ impl EffectComponent {
             let parameters_container = effect_guard.parameters().unwrap().parameters_f32().unwrap();
             let parameters = parameters_container
                 .iter()
-                .map(|p| {
+                .enumerate()
+                .map(|(i, p)| {
+                    let map = KeyCode::Char(char::from_digit(i as u32 + 1, 10).unwrap());
                     make_shared(ParameterComponentF32::from_named(
                         p,
                         Direction::Vertical,
                         20,
                         oosc_core::utils::interpolation::InterpolateMethod::Linear,
-                        KeyCode::Char('1'),
+                        map,
                     )) as Shared<dyn FocusableComponent>
                 })
                 .collect::<Vec<Shared<dyn FocusableComponent>>>();
@@ -70,13 +74,25 @@ impl Component for EffectComponent {
             .border_type(BorderType::Rounded)
             .title_alignment(Alignment::Center);
         f.render_widget(b, layout.rect);
-        self.parameters.draw_in_layout(f, &[layout.inner])?;
+        self.parameters.draw_in_layout(f, &layout.inner)?;
         Ok(())
     }
 
     fn resize(&mut self, rect: ratatui::prelude::Rect) -> anyhow::Result<()> {
-        let inner = rect.inner(&Margin::new(1, 1));
-        self.parameters.resize_in_layout(&[inner, inner])?;
+        let inner = {
+            let inner = rect.inner(&Margin::new(1, 1));
+            let len = self.parameters.components.len();
+            let size = 100 / len;
+            Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    std::iter::repeat_with(|| Constraint::Percentage(size as u16))
+                        .take(len)
+                        .collect::<Vec<_>>(),
+                )
+                .split(inner)
+        };
+        self.parameters.resize_in_layout(&inner)?;
         self.layout = Some(EffectLayout { rect, inner });
         Ok(())
     }
