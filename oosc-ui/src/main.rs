76aaac1a6274cf72ad8ad5ae15ad98344b72680b
karasikq <romastepanuik@gmail.com>
2024-02-21 11:46:44 +0300

    Wavetables

--- oosc-ui/src/main.rs
+++ oosc-ui/src/main.rs
@@ -5,6 +5,7 @@ use self::app::application::Application;
 use anyhow::Error;
 use cpal::traits::StreamTrait;
 use midly::Smf;
+use oosc_core::core::oscillator::WavetableOscillator;
 
 fn main() -> Result<(), Error> {
     let mut app = Application::new()?;
@@ -17,6 +18,40 @@ fn main() -> Result<(), Error> {
         midi_control.load(smf);
         // midi_control.set_bpm(69.0);
         midi_control.play();
+    }
+
+    {
+        let syn = app.ctx.synthesizer.lock().unwrap();
+        let mut oscs = syn.get_oscillators::<WavetableOscillator>();
+        let osc = oscs.next().unwrap();
+        let mut osc = osc.write().unwrap();
+        let osc = osc
+            .as_any_mut()
+            .downcast_mut::<WavetableOscillator>()
+            .unwrap();
+        osc.wavetable()
+            .write()
+            .unwrap()
+            .load_from(
+                "./oosc-ui/test-wavetables/2/SX 09.wav",
+                2048,
+            )
+            .unwrap();
+
+        let osc1 = oscs.next().unwrap();
+        let mut osc1 = osc1.write().unwrap();
+        let osc1 = osc1
+            .as_any_mut()
+            .downcast_mut::<WavetableOscillator>()
+            .unwrap();
+        osc1.wavetable()
+            .write()
+            .unwrap()
+            .load_from(
+                "./oosc-ui/test-wavetables/2/OB2v 01.wav",
+                2048,
+            )
+            .unwrap();
     }
 
     let stream = app.detach_stream()?;
